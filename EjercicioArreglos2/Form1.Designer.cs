08c8e9a1390870971758761521c08071a334b565
kolariki <ivankolariki1990@gmail.com>
2022-10-23 19:28:59 -0300

    cambios

---
+++ EjercicioArreglos2/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace EjercicioArreglos2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrecios = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPrecios
+            // 
+            this.btnPrecios.Location = new System.Drawing.Point(306, 207);
+            this.btnPrecios.Name = "btnPrecios";
+            this.btnPrecios.Size = new System.Drawing.Size(138, 23);
+            this.btnPrecios.TabIndex = 0;
+            this.btnPrecios.Text = "VER PRECIOS";
+            this.btnPrecios.UseVisualStyleBackColor = true;
+            this.btnPrecios.Click += new System.EventHandler(this.btnPrecios_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPrecios);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrecios;
+    }
+}
+
---
+++ EjercicioArreglos2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjercicioArreglos2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrecios_Click(object sender, EventArgs e)
+        {
+            int[] precios;
+
+
+            //crear en memoria
+            precios = new int[8];
+            double acumulado = 0;
+
+            for (int i = 0; i < precios.Length; i++)
+            {
+                int numero = Convert.ToInt32(Interaction.InputBox("Ingrese precios: "));
+                precios[i] = numero;
+                acumulado = acumulado + numero;
+            }
+
+            MessageBox.Show("La suma total de precios ingresados es: " + acumulado);
+
+        }
+    }
+}
