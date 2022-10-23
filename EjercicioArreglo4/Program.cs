f3dffc771036a0c2ee7881fab099fe1d9f08bd2a
kolariki <ivankolariki1990@gmail.com>
2022-10-23 19:19:58 -0300

    cambios

---
+++ EjercicioArreglo4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaVector10
+{
+    class PruebaVector10
+    {
+        private string[] nombres;
+        private int[] edades;
+
+        public void Cargar()
+        {
+            nombres = new string[5];
+            edades = new int[5];
+            for (int f = 0; f < nombres.Length; f++)
+            {
+                Console.Write("Ingrese nombre:");
+                nombres[f] = Console.ReadLine();
+                Console.Write("Ingrese edad:");
+                string linea;
+                linea = Console.ReadLine();
+                edades[f] = int.Parse(linea);
+            }
+        }
+
+        public void MayoresEdad()
+        {
+            Console.WriteLine("Personas mayores de edad.");
+            for (int f = 0; f < nombres.Length; f++)
+            {
+                if (edades[f] >= 18)
+                {
+                    Console.WriteLine(nombres[f]);
+                }
+            }
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            PruebaVector10 pv = new PruebaVector10();
+            pv.Cargar();
+            pv.MayoresEdad();
+        }
+    }
+}
